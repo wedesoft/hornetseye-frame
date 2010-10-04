63dceff232f1a2e3253f8d595e0062f40c607211
Jan Wedekind <jan@wedesoft.de>
2010-10-04 11:00:26 +0100

    Using libavformat include directory

--- ext/colourspace.cc
+++ ext/colourspace.cc
@@ -14,8 +14,7 @@
    You should have received a copy of the GNU General Public License
    along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 extern "C" {
-  // #include <libswscale/swscale.h>
-  #include <ffmpeg/swscale.h>
+  #include <libswscale/swscale.h>
 }
 #undef RSHIFT
 #include "colourspace.hh"
