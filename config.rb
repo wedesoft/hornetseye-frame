db0ce46e70dde6200dc5da81a1c783858c79880f
Jan Wedekind (Dr) <jan@wedesoft.de>
2025-06-30 12:33:57 +0100

    Update version number

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'rake'
 
 PKG_NAME = 'hornetseye-frame'
-PKG_VERSION = '1.1.3'
+PKG_VERSION = '1.1.4'
 CFG = RbConfig::CONFIG
 CXX = ENV[ 'CXX' ] || 'g++'
 RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
